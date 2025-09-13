52437ba0070cab877258cf0a7dd9a46b1b5d0a81
Joe-Roser <joeroser@gmail.com>
2025-09-13 18:39:29 +0100

    Day 3 - Allocating a Backbuffer

--- source/main.cpp
+++ source/main.cpp
@@ -1,14 +1,76 @@
 // Following Handmade Hero, The Molly Rocket series
 //
+
 #include <iostream>
-
+#include <windef.h>
 #include <windows.h>
+#include <wingdi.h>
 
+#define local_persist static
+#define global_variable static
+#define internal static
+
+// Global variables
+global_variable bool running;
+global_variable BITMAPINFO bitmapInfo;
+global_variable void *bitmapMemory;
+global_variable HBITMAP bitmapHandle;
+
+
+// Functions:
+//
+
+// Required since lldb has no support for listening to OutputDebugString()
 void DebugString(std::string dbg)
 {
   std::cout << dbg;
 }
 
+// Used to resize the window
+internal void WindowsResizeDibSection(int width, int height)
+{
+  //TODO: Bulletproof this
+  // Maybe free after
+
+  HDC bitmapDeviceContext;
+
+  if (bitmapHandle) {
+    DeleteObject(bitmapHandle);
+  }
+
+  if(!bitmapDeviceContext)
+  {
+    // TODO: Should these ever be recreated
+    bitmapDeviceContext = CreateCompatibleDC(0);
+  }
+
+  bitmapInfo.bmiHeader.biSize = sizeof(bitmapInfo.bmiHeader);
+  bitmapInfo.bmiHeader.biWidth = width;
+  bitmapInfo.bmiHeader.biHeight = height;
+  bitmapInfo.bmiHeader.biPlanes = 1;
+  bitmapInfo.bmiHeader.biBitCount = 32;
+  bitmapInfo.bmiHeader.biCompression = BI_RGB;
+
+  bitmapHandle = CreateDIBSection(bitmapDeviceContext,
+                                          &bitmapInfo,
+                                          DIB_RGB_COLORS,
+                                          &bitmapMemory,
+                                          0, 0);
+}
+
+// Used to update the image on the window
+internal void WindowsUpdateWindow(HDC deviceContext, int x, int y, int width, int height)
+{
+  StretchDIBits(deviceContext,           // Device context
+                x, y, width, height,     // Old dimensions
+                x, y, width, height,     // New dimensions
+                bitmapMemory,      // 
+                &bitmapInfo, // 
+                DIB_RGB_COLORS,          // Type of colours
+                SRCCOPY);                // What type of bitwise operations do I want to use
+}
+
+// Handles the messages from windows
 LRESULT CALLBACK MainWindowCallBack(HWND window,   // Window handle
                                     UINT msg,      // The message
                                     WPARAM wParam, //
@@ -18,13 +80,22 @@ LRESULT CALLBACK MainWindowCallBack(HWND window,   // Window handle
 
   switch (msg) {
     case WM_SIZE: {
+      RECT clientRect;
+      GetClientRect(window, &clientRect);
+      int height = clientRect.bottom - clientRect.top;
+      int width = clientRect.right - clientRect.left;
+
+      WindowsResizeDibSection(width, height);
+
       DebugString("WM_SIZE\n");
     } break;
     case WM_DESTROY: {
-      DebugString("WM_DESTROY\n");
+      // TODO: Handle this as an error
+      running = false;
     } break;
     case WM_CLOSE: {
-      DebugString("WM_CLOSE\n");
+      // TODO: Handle with message to user
+      running = false;
     } break;
     case WM_ACTIVATEAPP: {
       DebugString("WM_ACTIVATEAPP\n");
@@ -33,9 +104,11 @@ LRESULT CALLBACK MainWindowCallBack(HWND window,   // Window handle
       PAINTSTRUCT paint;
       HDC deviceContext = BeginPaint(window, &paint);
 
-      LONG height = paint.rcPaint.bottom - paint.rcPaint.top;
-      LONG width = paint.rcPaint.right - paint.rcPaint.left;
-      PatBlt(deviceContext, paint.rcPaint.left, paint.rcPaint.top, width, height, WHITENESS);
+      int x = paint.rcPaint.left;
+      int y = paint.rcPaint.top;
+      int height = paint.rcPaint.bottom - paint.rcPaint.top;
+      int width = paint.rcPaint.right - paint.rcPaint.left;
+      WindowsUpdateWindow(deviceContext, x, y, width, height);
 
       EndPaint(window, &paint);
     }
@@ -48,10 +121,12 @@ LRESULT CALLBACK MainWindowCallBack(HWND window,   // Window handle
   return result;
 }
 
+// The entrypoint to the app
 int CALLBACK WinMain(_In_ HINSTANCE instance,     // The Window Instance
                      _In_ HINSTANCE prevInstance, // The Previous Instance
                      _In_ LPSTR cmdLine,          // The Command Line
                      _In_ int nCmdShow) {         // Show Code??
+
 
   WNDCLASS windowClass = {};
 
@@ -83,10 +158,11 @@ int CALLBACK WinMain(_In_ HINSTANCE instance,     // The Window Instance
                                        0);                               // Pointer to value to be passed to created window
     if (windowHandle != NULL)
     {
-      // Message Loop from windows
-      MSG message;
-      for(;;)
+      // Calls message loop from windows
+      running = true;
+      while(running)
       {
+        MSG message;
         BOOL messageResult = GetMessage(&message, 0, 0, 0);
         if (messageResult > 0)
         {
