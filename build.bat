b38f141d4e1a3fa2988ba1d60cc70c654216b7d6
Joe-Roser <joeroser@gmail.com>
2025-09-13 16:17:04 +0100

    Day 2

--- build.bat
+++ build.bat
@@ -2,6 +2,6 @@
 if not exist .\build mkdir .\build
 pushd .\build
 
-zig c++ -g ../source/main.cpp
+zig c++ -g ../source/main.cpp -l gdi32 -o main.exe
 
 popd
--- source/main.cpp
+++ source/main.cpp
@@ -1,12 +1,111 @@
 // Following Handmade Hero, The Molly Rocket series
 //
+#include <iostream>
 
 #include <windows.h>
 
-int CALLBACK WinMain(_In_ HINSTANCE hInstance, _In_ HINSTANCE hPrevInstance,
-                     _In_ LPSTR lpCmdLine, _In_ int nCmdShow) {
+void DebugString(std::string dbg)
+{
+  std::cout << dbg;
+}
 
-  WNDCLASS WindowClass = {};
+LRESULT CALLBACK MainWindowCallBack(HWND window,   // Window handle
+                                    UINT msg,      // The message
+                                    WPARAM wParam, //
+                                    LPARAM lParam) //
+{
+  LRESULT result = 0;
+
+  switch (msg) {
+    case WM_SIZE: {
+      DebugString("WM_SIZE\n");
+    } break;
+    case WM_DESTROY: {
+      DebugString("WM_DESTROY\n");
+    } break;
+    case WM_CLOSE: {
+      DebugString("WM_CLOSE\n");
+    } break;
+    case WM_ACTIVATEAPP: {
+      DebugString("WM_ACTIVATEAPP\n");
+    } break;
+    case WM_PAINT: {
+      PAINTSTRUCT paint;
+      HDC deviceContext = BeginPaint(window, &paint);
+
+      LONG height = paint.rcPaint.bottom - paint.rcPaint.top;
+      LONG width = paint.rcPaint.right - paint.rcPaint.left;
+      PatBlt(deviceContext, paint.rcPaint.left, paint.rcPaint.top, width, height, WHITENESS);
+
+      EndPaint(window, &paint);
+    }
+    default: {
+      // DebugStringA("default\n");
+      result = DefWindowProc(window, msg, wParam, lParam);
+    } break;
+  }
+
+  return result;
+}
+
+int CALLBACK WinMain(_In_ HINSTANCE instance,     // The Window Instance
+                     _In_ HINSTANCE prevInstance, // The Previous Instance
+                     _In_ LPSTR cmdLine,          // The Command Line
+                     _In_ int nCmdShow) {         // Show Code??
+
+  WNDCLASS windowClass = {};
+
+  // TODO: Check thesee things still matter
+  windowClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
+  windowClass.lpfnWndProc = MainWindowCallBack;
+  // WindowClass.cbClsExtra = ;
+  // WindowClass.cbWndExtra = ;
+  windowClass.hInstance = instance;
+  // WindowClass.hIcon = ;
+  // WindowClass.hCursor = ;
+  // WindowClass.hbrBackground = ;
+  // WindowClass.lpszMenuName = ;
+  windowClass.lpszClassName = "HandmadeHeroWindowClass";
+
+
+  if (RegisterClass(&windowClass)) {
+    HWND windowHandle = CreateWindowEx(0,                                // Window style
+                                       windowClass.lpszClassName,        // Window class name
+                                       "Handmade Hero",                  // Window name
+                                       WS_OVERLAPPEDWINDOW | WS_VISIBLE, // The style of window created
+                                       CW_USEDEFAULT,                    // initial x
+                                       CW_USEDEFAULT,                    // initial y
+                                       CW_USEDEFAULT,                    // width
+                                       CW_USEDEFAULT,                    // height
+                                       0,                                // Handle to the parent or owner window
+                                       0,                                // Handle to a menu
+                                       instance,                         // handle of the instance
+                                       0);                               // Pointer to value to be passed to created window
+    if (windowHandle != NULL)
+    {
+      // Message Loop from windows
+      MSG message;
+      for(;;)
+      {
+        BOOL messageResult = GetMessage(&message, 0, 0, 0);
+        if (messageResult > 0)
+        {
+          TranslateMessage(&message);
+          DispatchMessage(&message);
+        }
+        else
+        {
+          break;
+        }
+      }
+    } else {
+      // TODO: Logging
+    DebugString("Uh oh, Failed to create window!!\n");
+    }
+  } else {
+    // TODO: Implement logging
+    DebugString("Uh oh, Faled to register class!!\n");
+  }
 
   return (0);
 }
