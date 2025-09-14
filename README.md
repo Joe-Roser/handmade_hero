8cc732d84c9d8859718399bab0666aee892f11d9
Joe-Roser <joeroser@gmail.com>
2025-09-14 21:50:47 +0100

    day 6 - Input handling

---
+++ README.md
@@ -0,0 +1,2 @@
+# Handmade Hero
+Following along with the Handmade Hero dev videos on Molly Rocket using [https://youtube.com/playlist?list=PLnuhp3Xd9PYTt6svyQPyRO_AAuMWGxPzU&si=mw_yQMSbkkdHzNMK | this series]. Commiting after every video.
--- source/windows_main.cpp
+++ source/windows_main.cpp
@@ -4,8 +4,7 @@
 #include <cstdint>
 #include <iostream>
 #include <windows.h>
-#include <wingdi.h>
-#include <winuser.h>
+#include <Xinput.h>
 
 #define local_persist static
 #define global_variable static
@@ -32,15 +31,48 @@ global_variable WindowsOffscreenBuffer globalBackBuffer;
 
 
 // Functions:
-//
+// 
+// 
+
+// Loading from OS
+// 
+
+// Load the windows functions into the program rather than referencing them
+// XInputGetState
+#define XINPUT_GET_STATE(name) DWORD WINAPI name(DWORD dwUserIndex, XINPUT_STATE *pState)
+typedef XINPUT_GET_STATE(xinput_get_state);
+XINPUT_GET_STATE(XInputGetStateStub) { return 0; }
+global_variable xinput_get_state *XInputGetState_ = XInputGetStateStub;
+#define XInputGetState XInputGetState_
+
+// XInputSetState
+#define XINPUT_SET_STATE(name) DWORD WINAPI name(DWORD dwUserIndex, XINPUT_VIBRATION *pVIBRATION)
+typedef XINPUT_SET_STATE(xinput_set_state);
+XINPUT_SET_STATE(XInputSetStateStub) { return 0; }
+global_variable xinput_set_state *XInputSetState_ = XInputSetStateStub;
+#define XInputSetState XInputSetState_
 
 // Required since lldb has no support for listening to OutputDebugString()
-void DebugString(std::string dbg)
+internal void debug_string(std::string dbg)
 {
     std::cout << dbg;
 }
 
-WindowsWindowDimension WindowsGetWindowDimenstion(HWND window)
+
+// Bring in the functions from xinput
+internal void windows_load_xinput()
+{
+    HMODULE xInputLibrary = LoadLibrary("xinput1_4.dll");
+
+    if(xInputLibrary)
+    {
+        XInputGetState = (xinput_get_state *)GetProcAddress(xInputLibrary, "XInputGetState");
+        XInputSetState = (xinput_set_state *)GetProcAddress(xInputLibrary, "XInputSetState");
+    }
+}
+
+// Calculate the window dimensions
+internal WindowsWindowDimension windows_get_window_dimenstion(HWND window)
 {
     RECT clientRect;
     GetClientRect(window, &clientRect);
@@ -52,7 +84,8 @@ WindowsWindowDimension WindowsGetWindowDimenstion(HWND window)
     return windowDimension;
 }
 
-internal void RenderGradient(WindowsOffscreenBuffer *buffer, int xOffset, int yOffset)
+// Render the weird gradient
+internal void render_gradient(WindowsOffscreenBuffer *buffer, int xOffset, int yOffset)
 {
     int width = buffer->width;
     int height = buffer->height;
@@ -77,8 +110,8 @@ internal void RenderGradient(WindowsOffscreenBuffer *buffer, int xOffset, int yO
     }
 }
 
-// Used to resize the window
-internal void WindowsResizeDibSection(WindowsOffscreenBuffer *buffer, int width, int height)
+// Resize window
+internal void windows_resize_dib_section(WindowsOffscreenBuffer *buffer, int width, int height)
 {
     //TODO: Bulletproof this
     // Maybe free after
@@ -106,15 +139,15 @@ internal void WindowsResizeDibSection(WindowsOffscreenBuffer *buffer, int width,
     buffer->memory = VirtualAlloc(0, bitmapMemorySize, MEM_COMMIT, PAGE_READWRITE);
 }
 
-// Used to update the image on the window
-internal void WindowsCopyBufToWin(HDC deviceContext, int width, int height, WindowsOffscreenBuffer buffer, int x, int y)
+// Overwrite the current buffer with a new buffer
+internal void windows_copy_buf_to_win(HDC deviceContext, int width, int height, WindowsOffscreenBuffer *buffer, int x, int y)
 {
     // TODO: Correct aspect ratio
     StretchDIBits(deviceContext,
                   0, 0, width, height,
-                  0, 0, buffer.width, buffer.height,
-                  buffer.memory,
-                  &buffer.info,
+                  0, 0, buffer->width, buffer->height,
+                  buffer->memory,
+                  &buffer->info,
                   DIB_RGB_COLORS,
                   SRCCOPY);
 }
@@ -140,19 +173,60 @@ LRESULT CALLBACK MainWindowCallBack(HWND window,   // Window handle
             running = false;
         } break;
         case WM_ACTIVATEAPP: {
-            DebugString("WM_ACTIVATEAPP\n");
+            debug_string("WM_ACTIVATEAPP\n");
+        } break;
+        case WM_SYSKEYDOWN: {
+        } break;
+        case WM_SYSKEYUP: {
+        } break;
+        case WM_KEYDOWN: {
+            uint32_t vKeyCode = wParam;
+            bool wasDown = ((lParam & (1 << 30)) != 0);
+            bool isDown = ((lParam & (1 << 31)) == 0);
+
+            if(wasDown != isDown){
+                if (vKeyCode == 'W') {
+                    debug_string("w\n");
+                } else if(vKeyCode == 'A') {
+                    debug_string("a\n");
+                } else if (vKeyCode == 'S') {
+                    debug_string("s\n");
+                } else if (vKeyCode == 'D') {
+                    debug_string("d\n");
+                } else if (vKeyCode == 'Q') {
+                    debug_string("q\n");
+                } else if (vKeyCode == 'E') {
+                    debug_string("e\n");
+                } else if (vKeyCode == VK_UP) {
+                    debug_string("up\n");
+                } else if (vKeyCode == VK_DOWN){
+                    debug_string("down\n");
+                } else if (vKeyCode == VK_LEFT){
+                    debug_string("left\n");
+                } else if (vKeyCode == VK_RIGHT){
+                    debug_string("right\n");
+                } else if (vKeyCode == VK_ESCAPE){
+                    debug_string("escape\n");
+                } else if (vKeyCode == VK_SPACE){
+                    debug_string("space\n");
+                }
+            }
+
+        } break;
+        case WM_KEYUP: {
+            uint32_t vKeyCode = wParam;
         } break;
         case WM_PAINT: {
             PAINTSTRUCT paint;
             HDC deviceContext = BeginPaint(window, &paint);
 
-            WindowsWindowDimension dimensions = WindowsGetWindowDimenstion(window);
+            WindowsWindowDimension dimensions = windows_get_window_dimenstion(window);
 
             int x = paint.rcPaint.left;
             int y = paint.rcPaint.top;
             int height = paint.rcPaint.bottom - paint.rcPaint.top;
             int width = paint.rcPaint.right - paint.rcPaint.left;
-            WindowsCopyBufToWin(deviceContext, dimensions.width, dimensions.height, globalBackBuffer, x, y);
+            windows_copy_buf_to_win(deviceContext, dimensions.width, dimensions.height, &globalBackBuffer, x, y);
 
             EndPaint(window, &paint);
         }
@@ -172,9 +246,11 @@ int CALLBACK WinMain(HINSTANCE instance,     // The Window Instance
                      int nCmdShow) {         // Show Code??
 
 
+    windows_load_xinput();
+
     WNDCLASS windowClass = {};
 
-    WindowsResizeDibSection(&globalBackBuffer, 1440, 720);
+    windows_resize_dib_section(&globalBackBuffer, 1440, 720);
 
     // TODO: Check thesee things still matter
     windowClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
@@ -210,6 +286,7 @@ int CALLBACK WinMain(HINSTANCE instance,     // The Window Instance
             running = true;
             while(running)
             {
+                // Message loop
                 MSG message;
                 while(PeekMessage(&message, 0, 0, 0, PM_REMOVE))
                 {
@@ -222,24 +299,62 @@ int CALLBACK WinMain(HINSTANCE instance,     // The Window Instance
                     DispatchMessage(&message);
                 }
 
-                RenderGradient(&globalBackBuffer, xOffset, yOffset);
+                // Controller input
+                for(DWORD controllerIndex = 0; controllerIndex < XUSER_MAX_COUNT; controllerIndex++){
+                    XINPUT_STATE controllerState;
+                    if(XInputGetState(controllerIndex, &controllerState) == ERROR_SUCCESS)
+                    {
+                        // Controller available
+                        XINPUT_GAMEPAD *pad = &controllerState.Gamepad;
+                        bool up = (pad->wButtons & XINPUT_GAMEPAD_DPAD_UP);
+                        bool down = (pad->wButtons & XINPUT_GAMEPAD_DPAD_DOWN);
+                        bool left = (pad->wButtons & XINPUT_GAMEPAD_DPAD_LEFT);
+                        bool right = (pad->wButtons & XINPUT_GAMEPAD_DPAD_RIGHT);
+                        bool start = (pad->wButtons & XINPUT_GAMEPAD_START);
+                        bool back = (pad->wButtons & XINPUT_GAMEPAD_BACK);
+                        bool lShoulder = (pad->wButtons & XINPUT_GAMEPAD_LEFT_SHOULDER);
+                        bool rShoulder = (pad->wButtons & XINPUT_GAMEPAD_RIGHT_SHOULDER);
+                        bool aButton = (pad->wButtons & XINPUT_GAMEPAD_A);
+                        bool bButton = (pad->wButtons & XINPUT_GAMEPAD_B);
+                        bool xButton = (pad->wButtons & XINPUT_GAMEPAD_X);
+                        bool yButton = (pad->wButtons & XINPUT_GAMEPAD_Y);
+
+                        int16_t stickX = pad->sThumbLX;
+                        int16_t stickY = pad->sThumbLY;
+
+                        if(aButton)
+                        {
+                            yOffset += 2;
+                            PXINPUT_VIBRATION vibration;
+                            vibration->wLeftMotorSpeed = 60000;
+                            vibration->wRightMotorSpeed = 60000;
+                            XInputSetState(controllerIndex, vibration);
+                        }
+                    }
+                    else
+                    {
+
+                    }
+                }
+
+                render_gradient(&globalBackBuffer, xOffset, yOffset);
 
                 {
                     HDC deviceContext = GetDC(window);
-                    WindowsWindowDimension dimension = WindowsGetWindowDimenstion(window);
+                    WindowsWindowDimension dimension = windows_get_window_dimenstion(window);
+                    windows_copy_buf_to_win(deviceContext, dimension.width, dimension.height, &globalBackBuffer, 0, 0);
 
-                    WindowsCopyBufToWin(deviceContext, dimension.width, dimension.height, globalBackBuffer, 0, 0);
                     xOffset++;
                     yOffset++;
                 }
             }
         } else {
             // TODO: Logging
-            DebugString("Uh oh, Failed to create window!!\n");
+            debug_string("Uh oh, Failed to create window!!\n");
         }
     } else {
         // TODO: Implement logging
-        DebugString("Uh oh, Faled to register class!!\n");
+        debug_string("Uh oh, Faled to register class!!\n");
     }
 
     return (0);
